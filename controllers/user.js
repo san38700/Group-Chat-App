2126a4aec324ef9d35dfdfe1d618b7df811a19eb
san38700 <sandeepkratosj@yahoo.in>
2024-03-07 19:21:25 +0530

    Integrating the frontend with backend

--- controllers/user.js
+++ controllers/user.js
@@ -4,15 +4,24 @@ const bcrypt = require('bcrypt')
 
 
 exports.createUser = async (req, res) => {
-    try {
+  try {
       const { name, email, number, password } = req.body;
-      const saltRounds = 10
-      const hashedPassword = await bcrypt.hash(password, saltRounds)
-      const user = await User.create({ name, email, number, password : hashedPassword });
-      console.log(user)
-      res.status(201).json({ users: user });
-    } catch (error) {
+
+      // Check if user with provided email already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+          return res.status(400).json({ error: 'User already exists. Please login.' });
+      }
+
+      // If user doesn't exist, hash the password and create the user
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const user = await User.create({ name, email, number, password: hashedPassword });
+
+      console.log(user);
+      res.status(201).json({ user });
+  } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Internal Server Error or User already exists' });
-    }
-  };
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+};

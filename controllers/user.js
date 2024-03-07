01321f568cce0a6b977ac0b8dc3065d4f3c595ed
san38700 <sandeepkratosj@yahoo.in>
2024-03-07 19:55:55 +0530

    Make the Login backend api

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 const User = require('../models/user')
-
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
 
 
@@ -23,5 +23,37 @@ exports.createUser = async (req, res) => {
   } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+function generateAccessToken(id,name,premiumuser){
+  const jwtToken = jwt.sign({userId : id, name : name},process.env.TOKEN_SECRET)
+  return jwtToken
+}
+
+exports.userLogin = async (req, res) => {
+  const { email, password } = req.body;
+        console.log(email)
+  try {
+      // Check if the user exists in the database
+      const users = await User.findOne({where: {email: email}});
+      console.log(users)
+
+      if (!users) {
+          return res.status(404).json({ message: '404 User not found Please Sign up' });
+      }
+
+      //check if password matches after decryption
+      const passwordMatch = await bcrypt.compare(password, users.password);
+
+      if (!passwordMatch) {
+        return res.status(401).json({ message: '401 User not authorized' });
+      }
+
+      res.json({user:users,jwtToken: generateAccessToken(users.id,users.name)})
+
+  } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
   }
 };
